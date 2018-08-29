573038985d3d730f18918a183722400081173353
Mario Monroy <m.monroyc22@gmail.com>
2018-08-29 15:16:18 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,23 +2,24 @@
 from setuptools import setup, find_packages
 import re, ast
 
+with open('requirements.txt') as f:
+	install_requires = f.read().strip().split('\n')
+
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
 with open('tree_view/__init__.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(
         f.read().decode('utf-8')).group(1)))
 
-requirements = parse_requirements("requirements.txt", session="")
-
 setup(
-	name='tree_view',
-	version=version,
-	description='Tree View Test',
-	author='MN Tech',
-	author_email='revant@revant.me',
-	packages=find_packages(),
-	zip_safe=False,
-	include_package_data=True,
-	install_requires=[str(ir.req) for ir in requirements],
-	dependency_links=[str(ir._link) for ir in requirements if ir._link]
+		name='tree_view',
+		version=version,
+		description='Tree View Test',
+		author='MN Tech',
+		author_email='revant@revant.me',
+		packages=find_packages(),
+		zip_safe=False,
+		include_package_data=True,
+		install_requires=install_requires
+		# dependency_links=[str(ir._link) for ir in requirements if ir._link]
 )
